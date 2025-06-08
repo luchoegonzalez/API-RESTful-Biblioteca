a04d2847970045f6de95272f7b2b6b40bbfeb61f
luchoegonzalez <legonzalez@dc.uba.ar>
2025-06-08 20:38:10 -0300

    creacion de metodo para obtener libros

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
+import Book from '../models/booksModel';
 
 const getBooks = async (req: Request, res: Response): Promise<any> => {
   try {
-    
+    const books = await Book.find();
+    res.status(200).json({
+      success: true,
+      data: books,
+      message: 'Books retrieved successfully'
+    });
   } catch (error) {
-    
+    const err = error as Error;
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
   }
 }
 
