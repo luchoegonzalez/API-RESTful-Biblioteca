62c5dc0b429b9056b21feee35d92bc3ad8d8edd3
luchoegonzalez <legonzalez@dc.uba.ar>
2025-06-09 00:12:59 -0300

    agrego respuesta por defecto al ir a otra URL

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json());
 app.use("/api/books", booksRouter);
 
+app.use("", (req, res) => {
+  res.json({success: false, message: "Source not found, go to /api/books"});
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   connectDB();
